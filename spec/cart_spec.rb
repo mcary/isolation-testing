8b91fc1f97e5fdd38a6800ae56220cd33709a362
Marcel M. Cary <marcel@oreilly.com>
2016-11-28 12:58:33 -0800

    Remove unneeded test for the default Cart constructor
    The test case for emptiness adequately captures that we can create a
    Cart, so there is no longer a need for that specific test case.
    
    Remove the test case to create a Cart without doing anything with it.

--- spec/cart_spec.rb
+++ spec/cart_spec.rb
@@ -1,10 +1,6 @@
 require_relative "../lib/cart"
 
 RSpec.describe Cart do
-  it "can be constructed" do
-    cart = Cart.new
-  end
-
   it "can be empty" do
     cart = Cart.new
     expect(cart.empty?).to be true
