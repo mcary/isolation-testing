65da388665e2b90f1230c857d049b9e3a4db48e1
Marcel M. Cary <marcel@oreilly.com>
2016-11-28 13:43:06 -0800

    Extract repeated setup from test cases
    The three examples all share the same first line of setup.
    
    Extract that to an RSpec "let".

--- spec/cart_spec.rb
+++ spec/cart_spec.rb
@@ -1,19 +1,18 @@
 require_relative "../lib/cart"
 
 RSpec.describe Cart do
+  let(:cart) { Cart.new }
+
   it "can be empty" do
-    cart = Cart.new
     expect(cart.empty?).to be true
   end
 
   it "can have items added to it" do
-    cart = Cart.new
     cart.add(Object.new)
     expect(cart.empty?).to be false
   end
 
   it "changes empty? when adding items" do
-    cart = Cart.new
     expect {
       cart.add(Object.new)
     }.to change {
