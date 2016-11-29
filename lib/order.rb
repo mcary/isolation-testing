a577beb882e70b27eee1c06f8c1659a97abf3a62
Marcel M. Cary <marcel@oreilly.com>
2016-11-29 10:00:23 -0800

    Accept payment for Orders
    Orders can be placed but not paid for.
    
    Add a mechanism to record payment for orders and determine whether
    they're paid or not.

--- lib/order.rb
+++ lib/order.rb
@@ -5,9 +5,18 @@ class Order
 
   def initialize(items)
     @items = items.dup
+    @payments = []
   end
 
   def amount_due
     @items.inject(0) { |sum, item| sum + item.price }
+  end
+
+  def accept_payment(amount)
+    @payments.push amount
+  end
+
+  def paid?
+    amount_due <= @payments.inject(0) { |sum, amt| sum + amt }
   end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -17,4 +17,20 @@ RSpec.describe Order do
     items.push PriceableStub.new(10)
     expect(order.amount_due).to eq 0
   end
+
+  it "accepts payment" do
+    order = Order.new([PriceableStub.new(10)])
+    expect {
+      order.accept_payment(10)
+    }.to change {
+      order.paid?
+    }.from(false).to(true)
+  end
+
+  it "accepts payment" do
+    order = Order.new([PriceableStub.new(10)])
+    expect(order.paid?).to be false
+    order.accept_payment(5)
+    expect(order.paid?).to be false
+  end
 end
