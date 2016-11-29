5d8f3566780acc3ff511c96673f6fd8b97a3dacf
Marcel M. Cary <marcel@oreilly.com>
2016-11-29 10:27:27 -0800

    Enhance CartView to render an ERB file

--- lib/cart_view.rb
+++ lib/cart_view.rb
@@ -1,5 +1,15 @@
 class CartView
+  def initialize
+    @erb = ERB.new(File.read(file_name))
+  end
+
   def render
-    ""
+    @erb.result(binding)
+  end
+
+  private
+
+  def file_name
+    File.expand_path("cart_view.erb", File.dirname(__FILE__))
   end
 end
--- spec/cart_view_spec.rb
+++ spec/cart_view_spec.rb
@@ -3,7 +3,7 @@ require_relative "../lib/cart_view"
 RSpec.describe CartView do
   it "renders" do
     result = CartView.new.render
-    expect(result).to eq ""
+    expect(result).to eq "Cart\n"
   end
 end
 
