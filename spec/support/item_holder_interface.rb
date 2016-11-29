303b90653e235243b726dd15260ae6f9a7f81388
Marcel M. Cary <marcel@oreilly.com>
2016-11-28 17:00:42 -0800

    Improve Priceable and ItemHolder specs
    The shared examples for ItemHolder and Priceable are not very robust.
    They check for responding to required methods, but they don't check
    return values.
    
    Actually call the methods and expect a hard-coded return for Priceable
    and expect an Array for ItemHolder.  Ideally we'd also examine the type
    of the Array contents, but because we are duck-typing, it's hard to
    invoke the ItemHolder shared example group on one of them.

--- spec/support/item_holder_interface.rb
+++ spec/support/item_holder_interface.rb
@@ -2,4 +2,8 @@ RSpec.shared_context "ItemHolder interface" do
   it "responds to #items" do
     object.respond_to? :items
   end
+
+  it "responds to #items with an array" do
+    expect(object.items).to be_a Array
+  end
 end
--- spec/support/priceable_interface.rb
+++ spec/support/priceable_interface.rb
@@ -2,4 +2,8 @@ RSpec.shared_context "Priceable interface" do
   it "responds to #price" do
     object.respond_to? :price
   end
+
+  it "returns the expected number from #price" do
+    expect(object.price).to eq 10
+  end
 end
